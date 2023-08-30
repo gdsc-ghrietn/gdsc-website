ce87ed2f7eeb00be3e254c0aaf13cae091175b74
Sahil Yeole <sahilyeole93@gmail.com>
2023-08-31 00:09:28 +0530

    fix vercel analytics
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
+import { Analytics } from '@vercel/analytics/react'
 import './App.css'
-import {inject} from '@vercel/analytics'
 
 function App() {
-
-inject()
 
   return (
     <>
     <div>
         <h1>GDSC-GHRIETN</h1>
     </div>
+    <Analytics/>
     </>
   )
 }
