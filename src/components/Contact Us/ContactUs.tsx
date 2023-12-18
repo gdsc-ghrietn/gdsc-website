e9a02a0134c7cd07159b3cbe090da416b883cd05
Sahil Yeole <73148455+sahilyeole@users.noreply.github.com>
2023-12-18 18:56:30 +0000

    add email api support

--- src/components/Contact Us/ContactUs.tsx
+++ src/components/Contact Us/ContactUs.tsx
@@ -1,9 +1,38 @@
 import './ContactUs.styles.css';
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
 
 const ContactUs: React.FC =()=>{
+    const form = useRef(null);
+    const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+    const sendEmail = (e:any) => {
+        e.preventDefault();
+    
+       form.current && emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
     return(
         <div id="contact-us-section">
             <h1>Contact Us</h1>
+            <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Subject</label>
+            <input type='text' name='subject' />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+            </form>
         </div>
     );
 }
