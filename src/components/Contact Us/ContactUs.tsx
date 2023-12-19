94c8de523dc566e1af44ff061e6a12254f14f418
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2023-12-19 11:28:15 +0530

    contact us  function working reset form added toastify ui

--- src/components/Contact Us/ContactUs.tsx
+++ src/components/Contact Us/ContactUs.tsx
@@ -1,27 +1,32 @@
 import './ContactUs.styles.css';
 import emailjs from '@emailjs/browser'
 import { useRef } from 'react'
+import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ContactUs: React.FC =()=>{
     const form = useRef(null);
     const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
     const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
     const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-
     const sendEmail = (e:any) => {
-        e.preventDefault();
-    
+        e.preventDefault(); 
        form.current && emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
           .then((result) => {
               console.log(result.text);
+                toast.success('Message sent successfully',{autoClose: 1000});
+            if (form.current) {
+                (form.current as HTMLFormElement).reset();
+            }
           }, (error) => {
               console.log(error.text);
-          });
+                toast.error('Message not sent',{autoClose: 1000});
+            });
       };
 
     return(
         <div id="contact-us-section">
-            <h1>Contact Us</h1>
+            <ToastContainer />
             <form ref={form} onSubmit={sendEmail}>
             <label>Name</label>
             <input type="text" name="user_name" />
