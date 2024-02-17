f5a33064a6f9ab72cf047d1f42ddfdb4c4c7ac0d
Sahil Yeole <sahilyeole93@gmail.com>
2024-02-18 01:27:29 +0530

    clean for redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Analytics } from '@vercel/analytics/react'
 import './App.css'
 
-import Homepage from './components/Homepage/Homepage'
 function App() {
   return (
     <>
       <div className="App">
-        <Homepage/>
       </div>
     <Analytics/>
     </>  
