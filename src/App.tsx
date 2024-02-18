dc340db2c0ff4be55b48ec26921fca91cbd1c766
Sahil Yeole <sahilyeole93@gmail.com>
2024-02-18 21:27:26 +0530

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Analytics } from '@vercel/analytics/react'
 import './App.css'
 import Home from './components/Home/Home'
+import Navbar from './components/Navbar/navbar'
 
 function App() {
     return (
         <>
             <div className="App">
+                <Navbar/>
                 <Home/>
             </div>
             <Analytics/>
--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,9 +1,20 @@
+import gdsclogo from "../../assets/gdsc_logo.png";
 
 const Navbar = () => {
     return (
-        <div className="w-full flex justify-center items-center">
-            <div className="w-[97%] h-[50px] bg-black mt-2 border-2 border-red-800 rounded-lg flex justify-between items-center px-4
-            ">
+        <div className="w-full flex justify-center items-center fixed top-0 z-10">
+            <div className="w-[97%] h-[45px] bg-black mt-4 rounded-[40px] flex justify-between items-center px-6 bg-opacity-80 text-white text-opacity-90 font-game1 text-[22px] py-4">
+                <div className="flex">
+                    <img src={gdsclogo} alt="GDSC Logo" width={50} height={10} />
+                    <div className="ml-4 hover:cursor-pointer">GDSC GHRIETN</div>
+                </div>
+                <div className="flex">
+                    <div className="hover:cursor-pointer">HOME</div>
+                    <div className="ml-6 hover:cursor-pointer">ABOUT</div>
+                    <div className="ml-6 hover:cursor-pointer">EVENTS</div>
+                    <div className="ml-6 hover:cursor-pointer">TEAM</div>
+                    <div className="ml-6 hover:cursor-pointer">CONTACT</div>
+                </div>
             </div>
         </div>
     )
