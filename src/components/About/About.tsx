014314f30f10e816ec59265b79388761ae0b250d
Sahil Yeole <sahilyeole93@gmail.com>
2024-02-19 00:24:45 +0530

    fix sections padding

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,7 @@
 
 const About = () => {
     return (
-        <div className="w-full h-screen flex justify-center items-top py-6 pt-20 border-4 border-white">
+        <div className="w-full h-screen flex justify-center items-top pt-24 border-4 border-white">
             <div className="text-6xl font-bold text-white font-game1">
                 About Us
             </div>
