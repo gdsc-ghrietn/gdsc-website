337a8b5c6b6dd266163a075b0e23f98cf24a3102
Sahil Yeole <sahilyeole93@gmail.com>
2024-02-18 22:13:38 +0530

    add empty sections with temp borders for screen ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Analytics } from '@vercel/analytics/react'
 import './App.css'
 import Home from './components/Home/Home'
-import Navbar from './components/Navbar/navbar'
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+import Events from './components/Events/Events'
+import Team from './components/Team/Team'
+import Contact from './components/Contact/Contact'
 
 function App() {
     return (
@@ -9,6 +13,10 @@ function App() {
             <div className="App">
                 <Navbar/>
                 <Home/>
+                <About/>
+                <Events/>
+                <Team/>
+                <Contact/>
             </div>
             <Analytics/>
         </>  
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+
+const Contact = () => {
+    return (
+        <div className="w-full h-screen flex justify-center items-top py-6 pt-20 border-4 border-white">
+            <div className="text-6xl font-bold text-white font-game1">
+                Contact Us
+            </div>
+        </div>
+    )
+}
+
+export default Contact
