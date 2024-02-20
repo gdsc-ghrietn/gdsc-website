f3018b695bc8f1da0298b1f72ed26f69bdfe21ec
Anukrati Mehta <anukratimehta@gmail.com>
2024-02-21 00:57:08 +0530

    About.tsx style updated

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,24 @@
 
 const About = () => {
     return (
-        <div className="w-full h-screen flex justify-center items-top pt-24 border-4 border-white">
-            <div className="text-6xl font-bold text-white font-game1">
+        <div className="w-full h-screen flex flex-col justify-evenly content-between uppercase">
+            <div className="text-2xl font-normal text-white font-game2 flex items-center justify-center">
                 About Us
+            </div>
+            <div className="flex flex-col max-w-[61%] pl-32 space-y-6">
+                <div className="text-2xl font-normal text-white font-game2">What's GDSC?</div>
+                <div className="text-2xl font-normal text-white font-game1">
+                    Google collaborates with university students who are passionate about growing developer communities. GDSC is an Organisation for university students who want to bring about a change through technology. We are focused upon building technical and non-technical skills, which would help students to build a better community.
+                </div>
+            </div>
+            <div className="flex flex-col max-w-[61%] pl-32 space-y-6">
+                <div className="text-2xl font-normal text-white font-game2">What GDSC GHRIETN does?</div>
+                <ul className="text-2xl font-normal text-white font-game1 list-disc list-inside pl-4">
+                    <li className="leading-none">Projects</li>
+                    <li className="leading-none">Seminars</li>
+                    <li className="leading-none">Hackathons</li>
+                    <li className="leading-none">Study Jams</li>
+                </ul>
             </div>
         </div>
     )
