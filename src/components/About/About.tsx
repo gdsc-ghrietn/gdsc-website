868e864a6c89cdf84d53a65f1dbfa57d9f45863b
Anukrati Mehta <anukratimehta@gmail.com>
2024-02-21 01:17:56 +0530

    fixed line height

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,19 +5,19 @@ const About = () => {
             <div className="text-2xl font-normal text-white font-game2 flex items-center justify-center">
                 About Us
             </div>
-            <div className="flex flex-col max-w-[61%] pl-32 space-y-6">
+            <div className="flex flex-col max-w-[61%] pl-20 space-y-6">
                 <div className="text-2xl font-normal text-white font-game2">What's GDSC?</div>
-                <div className="text-2xl font-normal text-white font-game1">
+                <div className="text-2xl font-normal text-white font-game1 leading-none">
                     Google collaborates with university students who are passionate about growing developer communities. GDSC is an Organisation for university students who want to bring about a change through technology. We are focused upon building technical and non-technical skills, which would help students to build a better community.
                 </div>
             </div>
-            <div className="flex flex-col max-w-[61%] pl-32 space-y-6">
+            <div className="flex flex-col max-w-[61%] pl-20 space-y-6">
                 <div className="text-2xl font-normal text-white font-game2">What GDSC GHRIETN does?</div>
-                <ul className="text-2xl font-normal text-white font-game1 list-disc list-inside pl-4">
-                    <li className="leading-none">Projects</li>
-                    <li className="leading-none">Seminars</li>
-                    <li className="leading-none">Hackathons</li>
-                    <li className="leading-none">Study Jams</li>
+                <ul className="leading-none text-2xl font-normal text-white font-game1 list-disc list-inside pl-4">
+                    <li>Projects</li>
+                    <li>Seminars</li>
+                    <li>Hackathons</li>
+                    <li>Study Jams</li>
                 </ul>
             </div>
         </div>
