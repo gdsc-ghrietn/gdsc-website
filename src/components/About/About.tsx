f134ae64ccaa961d0e21191a8dc0afa60cd2448a
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-02-27 16:14:37 +0530

    fix to original

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,8 +5,8 @@ const About = () => {
         <div className="relative w-full min-h-screen md:h-screen flex flex-col justify-evenly content-between uppercase ">
             <div className="text-xl lg:text-2xl font-normal text-white font-game2 flex items-center justify-center">
                 About Us
-            </div>
-            <div className="flex flex-col lg:max-w-[75%] xl:max-w-[61%] px-7 md:px-10 lg:pl-20 space-y-6">
+            </div> 
+            <div className="flex flex-col lg:max-w-[75%] xl:max-w-[61%] px-7 md:px-10 lg:pl-20 space-y-6"> 
                 <div className="text-xl lg:text-2xl font-normal text-white font-game2">What's GDSC?</div>
                 <div className="text-xl lg:text-[1.5rem] font-normal text-white font-game1 leading-none">
                     Google collaborates with university students who are passionate about growing developer communities. GDSC is an Organisation for university students who want to bring about a change through technology. We are focused upon building technical and non-technical skills, which would help students to build a better community.
@@ -21,7 +21,7 @@ const About = () => {
                     <li>Study Jams</li>
                 </ul>
             </div>
-            <BackgroundBeams/>
+            <BackgroundBeams/>            
         </div>
 
     )
