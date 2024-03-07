972a1bfae38cf38f02b66eb25e05fbb0a97b80a3
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-03-07 19:46:44 +0530

    temmember card

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,9 +1,23 @@
+import teamMebersData from "./Team.json";
+import TeamMemberCard from "./TeamMemberCard";
+interface TeamMember {
+    id: number;
+    name: string;
+    role: string;
+    image_url: string;
+    github?: string;
+    linkedin?: string;
+  }
+
 const Team = () => {
+    const member: TeamMember = teamMebersData[14];
     return (
         <div className="w-full h-screen flex justify-center items-top pt-24 border-4 border-white">
             <div className="text-6xl font-bold text-white font-game1">
                 Meet Our Team
             </div>
+                
+            <TeamMemberCard key={member.id} image_url={member.image_url} fullName={member.name} position={member.role} github={member.github} linkedin={member.linkedin} />
         </div>
     )
 }
