d046104a45900f106f736ee124ea37b160adbcf9
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-03-13 00:18:55 +0530

    link in team section removed

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,15 +1,9 @@
-import { Link } from 'react-router-dom'
 const Team = () => {
     return (
         <div className="w-full h-screen flex justify-center items-top pt-24 border-4 border-white">
             <div className="text-6xl font-bold text-white font-game1">
                 Meet Our Team
             </div>
-            <Link to="/team">
-                <div className="text-2xl font-bold text-white font-game1">
-                    Learn More
-                </div>
-            </Link>
         </div>
     )
 }
