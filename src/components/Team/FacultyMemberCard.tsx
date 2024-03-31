3009d3c72a2b7617e9784f94494ae30a046816ef
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-03-31 17:04:23 +0530

    team member image added and some bug fixed

--- src/components/Team/FacultyMemberCard.tsx
+++ src/components/Team/FacultyMemberCard.tsx
@@ -12,7 +12,7 @@ export interface FacultyMemberProps {
 const FacultyMemberCard: React.FC<FacultyMemberProps> = ({ image_url,name, role, linkedin }) => {
   return (
     <div className="w-25 pb-2 bg-white flex flex-col items-center rounded-sm  shadow-[4px__3px_0px] shadow-red-600 m-6 ">
-      <img src={image_url} alt="team member" className="w-[90%] h-[50%] mt-2 rounded-lg" />
+      <img src={image_url} alt="team member" className="w-[90%] h-[40%] mt-2 rounded-lg" />
       <div className="flex flex-col items-center h-[60%] w-full justify-center">
         <h2 className="text-2xl font-game1 w-full ml-8 flex word-wrap text-black">{name}</h2>
         <h2 className="text-lg font-game1 ml-8 word-wrap w-full text-gray-500">{role}</h2>
